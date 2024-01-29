144480abcc0cf62d590cc6a5c2c2e0bde8804cd2
Ashwini Maurya <mauryaashvini99@gmail.com>
2024-01-29 11:35:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let gameResult = document.getElementById("gameResult");
+let userInput = document.getElementById("userInput");
+let randomNumber = Math.ceil(Math.random()*100);
+console.log( randomNumber);
+
+
+function checkGuess() {
+    let guessedNumber = parseInt(userInput.value);
+
+    
+
+    if (guessedNumber > randomNumber) {
+        gameResult.textContent = "Too High! Try Again!";
+        gameResult.style.backgroundColor = "#1e217c";
+    }
+
+    else if  (guessedNumber < randomNumber) {
+        gameResult.textContent = "Too low! Try Again!";
+        gameResult.style.backgroundColor = "#1e217c";
+    }
+
+    
+    else if  (guessedNumber === randomNumber) {
+        gameResult.textContent = "Congratulations!! You Got It!!!";
+        gameResult.style.backgroundColor = "green" ;
+    }
+
+    else {
+        gameResult.textContent = "Provide a valid user Input.";
+        gameResult.style.backgroundColor = "red";
+    }
+}
